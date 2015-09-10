4bee2ccce3ac24dbc200b68933e626f5328e9606
Neder <ocw5902@naver.com>
2015-09-11 01:16:48 +0900

    TEST????

--- js/twpf.js
+++ js/twpf.js
@@ -3,4 +3,6 @@ alert(ref); // TEST
 if (ref.match("/https:\/\/t.co(.*)/i")){ // Twitter t.co URL match Regex
     jQuery('#twpf').show();
 	alert("Twitter!"); // TEST
+} else {
+	alert("Not Twitter!"); // TEST
 }
