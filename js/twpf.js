9326bb3eff00e0be624b80e3c51c88d9a419b550
Neder <ocw5902@naver.com>
2015-09-11 01:15:46 +0900

    TEST???

--- js/twpf.js
+++ js/twpf.js
@@ -1,5 +1,5 @@
-alert("Loaded!"); // TEST
 var ref = document.referrer;
+alert(ref); // TEST
 if (ref.match("/https:\/\/t.co(.*)/i")){ // Twitter t.co URL match Regex
     jQuery('#twpf').show();
 	alert("Twitter!"); // TEST
