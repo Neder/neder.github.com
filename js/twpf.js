dd52a605208776a4a22ceb04cb1d21c7ee1eae1f
Neder <ocw5902@naver.com>
2015-09-11 01:39:00 +0900

    include jquery

--- js/twpf.js
+++ js/twpf.js
@@ -1,7 +1,4 @@
 var ref = document.referrer;
 if (ref.match(/https:\/\/t.co(.*)/i)) { // Twitter t.co URL match Regex
-	alert("Twitter!");
     jQuery('#twpf').show();
-} else {
-	alert("Not Twitter!");
 }
