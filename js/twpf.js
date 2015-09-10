f429257b7ce7d5f8079dc4e04a0a5cc78a77f061
Neder <ocw5902@naver.com>
2015-09-11 01:32:35 +0900

    OMG

--- js/twpf.js
+++ js/twpf.js
@@ -1,5 +1,7 @@
 var ref = document.referrer;
-if (ref.match(/https:\/\/t.co(.*)/i)){ // Twitter t.co URL match Regex
+if (ref.match(/https:\/\/t.co(.*)/i)) { // Twitter t.co URL match Regex
+	alert("Twitter!");
     jQuery('#twpf').show();
 } else {
+	alert("Not Twitter!");
 }
