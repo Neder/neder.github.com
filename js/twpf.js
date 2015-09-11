56401d38ded48061c6d59671f98bd9dde67e31cb
Neder <ocw5902@naver.com>
2015-09-11 22:39:49 +0900

    Support more browsers

--- js/twpf.js
+++ js/twpf.js
@@ -1,4 +1,6 @@
-var ref = document.referrer;
-if (ref.match(/http(s?):\/\/t.co(.*)/i)) { // Twitter t.co URL match Regex
-    jQuery('#twpf').show();
-}
+$(document).ready(function(){
+	var ref = document.referrer;
+	if (ref.match(/http(s?):\/\/t.co(.*)/i)) { // Twitter t.co URL match Regex
+		jQuery('#twpf').show();
+	}
+});
